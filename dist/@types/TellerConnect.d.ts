03ce14954ffa648318d25aff1a017a6b84355f95
Mauricio Guzman <122919841+finally-mauricio@users.noreply.github.com>
2024-02-28 11:23:49 -0400

    Update TellerConnect.d.ts

--- dist/@types/TellerConnect.d.ts
+++ dist/@types/TellerConnect.d.ts
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import { TellerConnectOptions } from './types';
 type PropTypes = TellerConnectOptions & React.HTMLProps<HTMLButtonElement>;
 export declare const TellerConnect: React.FC<PropTypes>;
