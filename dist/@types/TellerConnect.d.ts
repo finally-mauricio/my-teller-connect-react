7de70b5c402ba8b3bd08f3cee48fc7fe9c5a79e2
mauricio <mauricio.guzmannova@finally.com>
2024-02-28 11:22:59 -0400

    Revert "update version"
    This reverts commit 547e7c61e2203510c91cefcffb135d2262f6098e.

---
+++ dist/@types/TellerConnect.d.ts
@@ -0,0 +1,5 @@
+import React from 'React';
+import { TellerConnectOptions } from './types';
+type PropTypes = TellerConnectOptions & React.HTMLProps<HTMLButtonElement>;
+export declare const TellerConnect: React.FC<PropTypes>;
+export {};
